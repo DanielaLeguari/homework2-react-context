b4f76a432d3b7872c97d0845f3d215bd80bb622c
Daniela Leguari <danielaleguari@outlook.com>
2022-11-16 23:16:37 -0300

    estilo login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,11 +3,12 @@ import { AuthUserContext } from '../../context/AuthUserContext';
 import { useForm } from "react-hook-form";
 import { IUserLogin } from '../../utils/interface';
 import styles from './Login.module.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const { login } = useContext(AuthUserContext);
   //form
-  const { register, handleSubmit} = useForm<IUserLogin>();
+  const { register, handleSubmit } = useForm<IUserLogin>();
 
   const doLogin = async (data: IUserLogin) => {
     await login(data);
@@ -16,15 +17,19 @@ const Login = () => {
 
   return (
     <main className={styles.containerFormLogin}>
-    <form className={styles.formLogin} onSubmit={handleSubmit(doLogin)}>
-      <label htmlFor="login">Login</label>
-      <input type="text" required id="login" {...register("login")} />
+      <form className={styles.formLogin} onSubmit={handleSubmit(doLogin)}>
+        <label htmlFor="login">Login</label>
+        <input type="text" required id="login" {...register("login")} />
 
-      <label htmlFor="senha">Senha</label>
-      <input type="text" required id="senha" {...register("senha")} />
-
-      <input type="submit" value="Entrar" />
-    </form>
+        <label htmlFor="senha">Senha</label>
+        <input type="password" required id="senha" {...register("senha")} />
+        <div className={styles.containerAcoes}>
+          <input className={styles.btnEntrar} type="submit" value="Entrar" />
+        </div>
+        <div>
+         <Link style={{textDecoration:"none", color:"var(--cor-fonte-login)"}} to={"/sign-up"}>Cadastre-se aqui!</Link>
+        </div>
+      </form>
     </main>
   );
 }
